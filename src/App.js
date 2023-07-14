9d8c1001df5487ba0edc858838f9a0ac3d61a10d
GianlucaBitocchi <sechh@live.com.ar>
2023-07-14 09:58:20 -0300

    primer comentario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
+
+import LogoGb from "./img/LogoGb.png";
+import Carrito from "./img/Carrito.png";
+
+import "bulma/css/bulma.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <figure class="image is-96x96">
+                    <NavBar img={LogoGb} />
+                </figure>
+
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+
+                    <a class="navbar-item">
+                        INICIO
+                    </a>
+
+                    <a class="navbar-item">
+                        PRODUCTOS
+                    </a>
+                </div>
+
+                <div>
+                <figure class="image is-64x64">
+                    <NavBar img={Carrito} />
+                </figure>
+                </div>
+                
+            </div>
+
+
+
+        </nav >
+
+
+    )
+
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar({ titulo , handle , img}){
+
+     
+    return (
+        <div>         
+            <img src={img} alt="logo"/>
+        </div>
+        
+    )
+}
+
+export default NavBar;
